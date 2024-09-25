3e66aff423328725893c1455658a5fb24eda89cb
Qnewek <dawidkusion2002@gmail.com>
2024-09-25 11:16:53 +0200

    Added some functions to droplet class and erosion class

--- ProceduralTerrainGenerator/ProceduralTerrainGenerator/src/terrainGeneration/Erosion.cpp
+++ ProceduralTerrainGenerator/ProceduralTerrainGenerator/src/terrainGeneration/Erosion.cpp
@@ -1,6 +1,7 @@
 #include "Erosion.h"
 
-
+#include <math.h>
+#include <random>
 
 namespace erosion {
 
@@ -11,9 +12,126 @@ namespace erosion {
 	Erosion::~Erosion()
 	{
 	}
-	
+
+	//Configuration functions
+	void Erosion::SetConfig(ErosionConfig config)
+	{
+		this->config = config;
+	}
+
+	void Erosion::Resize(int width, int height)
+	{
+		this->width = width;
+		this->height = height;
+	}
+
+	void Erosion::SetIterations(int iterations)
+	{
+		this->iterations = iterations;
+	}
+
+	int* Erosion::getIterationsRef()
+	{
+		return &iterations;
+	}
+
+	ErosionConfig* Erosion::getConfigRef()
+	{
+		return &config;
+	}
+
+	//Terraforming functions
 	void Erosion::Erode(float* map)
 	{
-		
+
+	}
+
+	vec2 Erosion::getGradient(float* map, vec2 pos)
+	{
+		vec2 gradient = { 0, 0 };
+		//Calculation offset of the droplet  on the (x,y)cell
+		float v = pos.x - static_cast<int>(pos.x);
+		float u = pos.y - static_cast<int>(pos.y);
+		//Get the integer part of the position ( x and y coordinates)
+		int x0 = static_cast<int>(pos.x);
+		int y0 = static_cast<int>(pos.y);
+
+		//It is assumed that erode function checks if the drop fell outside the map
+		//Formula used: g(pos) = ( (P(x+1, y) - P(x, y)) * (1 - v) + (P(x+1, y+1) - P(x, y+1)) * v )
+		//						 ( (P(x, y+1) - P(x, y)) * (1 - u) + (P(x+1, y+1) - P(x+1, y)) * u )
+		gradient.x = (map[y0 * width + x0 + 1] - map[y0 * width + x0]) * (1 - v) +
+					 ((map[(y0 + 1) * width + x0 + 1] - map[(y0 + 1) * width + x0]) * v);
+		gradient.y = (map[(y0 + 1) * width + x0] - map[y0 * width + x0]) * (1 - u) +
+					 ((map[(y0 + 1) * width + x0 + 1] - map[y0 * width + x0 + 1]) * u);
+
+		return gradient;
+	}
+
+	float Erosion::getElevationDifference(float* map, vec2 posOld, vec2 posNew)
+	{
+		//Calculate the difference in elevation between the old and new position of the droplet
+		//Formula used: elevationDifference = elevation(posNew) - elevation(posOld)
+		//Its calculated to determine whether the droplet is moving uphill or downhill
+		//If the difference is positive, the droplet is moving uphill
+		//If the difference is negative, the droplet is moving downhill
+		int x0 = static_cast<int>(posOld.x);
+		int y0 = static_cast<int>(posOld.y);
+		int x1 = static_cast<int>(posNew.x);
+		int y1 = static_cast<int>(posNew.y);
+
+		return map[y0 * width + x0] - map[y1 * width + x1];
+	}
+
+	//Droplet class functions
+	Droplet::Droplet(vec2 position, float velocity, float water, float sediment, float capacity) : position(position), velocity(velocity), water(water), sediment(sediment), capacity(capacity)
+	{
+	}
+
+	Droplet::~Droplet()
+	{
+	}
+
+	void Droplet::adjustDirection(vec2 gradient, float inertia)
+	{
+		//Calculate the direction of the droplet using the formula: 
+		//direction(new) = direction(old) * inertia + gradient * (1 - inertia)
+		//New direction is obtained by blending old position with the gradient, with inertia parameter
+		//being taken into account as blending strength parameter
+		//Inertia being close to 1 means the droplet will move in the same direction as before
+		//Inertia being close to 0 means the droplet will move in the direction of the gradient
+
+		float dx = direction.x * inertia + gradient.x * (1 - inertia);
+		float dy = direction.y * inertia + gradient.y * (1 - inertia);
+
+		//If the direction is zero which means droplet wouldnt move, generate a random direction
+		if (dx == 0 && dy == 0)
+		{
+			std::random_device rd;
+			std::mt19937 gen(rd());
+			std::uniform_real_distribution<float> dist(-1.0f, 1.0f);
+
+			dx = dist(gen);
+			dy = dist(gen);
+		}
+
+		//Normalize the direction to get a unit vector
+		float length = sqrtf((dx * dx) + (dy * dy));
+
+		if (length > 0)
+		{
+			direction.x = dx / length;
+			direction.y = dy / length;
+		}
+		this->adjustPosition();
+	}
+
+	void Droplet::adjustPosition()
+	{
+		//Update the position of the droplet using the formula:
+		//posion(new) = position(old) + direction(new)
+		//Formula does not take into account the velocity of the droplet in order not to "jump" over the cells
+		//This function performs one step which is not representing the real movement of the droplet in a time frame
+		position.x += direction.x;
+		position.y += direction.y;
 	}
 }
--- ProceduralTerrainGenerator/ProceduralTerrainGenerator/src/terrainGeneration/Erosion.h
+++ ProceduralTerrainGenerator/ProceduralTerrainGenerator/src/terrainGeneration/Erosion.h
@@ -1,16 +1,29 @@
 #pragma once
 
+//http://www.firespark.de/resources/downloads/implementation%20of%20a%20methode%20for%20hydraulic%20erosion.pdf
 
 namespace erosion {
 
 	struct ErosionConfig {
 		//Erosion parameters
-		int iterations = 1000;
 		float erosionRate = 0.1;
 		float sedimentCapacity = 0.01;
 		float minSlope = 0.01;
 		float gravity = 4.0;
 		float inertia = 0.1;
+		float depositionRate = 0.1;
+		float evaporationRate = 0.01;
+		//Initial values
+		float initialWater = 1.0;
+		float initialSediment = 0.0;
+		float initialVelocity = 0.0;
+		float initialCapacity = 0.01;
+		float initialSlope = 0.01;
+		float initialInertia = 0.1;
+	};
+
+	struct vec2 {
+		float x, y;
 	};
 
 	class Erosion
@@ -20,7 +33,51 @@ namespace erosion {
 		~Erosion();
 
 		void Erode(float* map);
+		vec2 getGradient(float* map, vec2 pos);
+		float getElevationDifference(float* map, vec2 posOld, vec2 posNew);
+
+		void SetConfig(ErosionConfig config);
+		void Resize(int width, int height);
+
+		void SetIterations(int iterations);
+
+
+		int* getIterationsRef();
+		ErosionConfig* getConfigRef();
+
 	private:
 		int width, height;
+		int iterations = 1000;
+		ErosionConfig config;
+	};
+
+	class Droplet
+	{
+	public:
+		Droplet(vec2 position, float velocity, float water = 0, float sediment = 0, float capacity);
+		~Droplet();
+
+		vec2 getPosition() { return position; }
+		vec2 getDirection() { return direction; }
+		float getVelocity() { return velocity; }
+		float getWater() { return water; }
+		float getSediment() { return sediment; }
+		float getCapacity() { return capacity; }
+
+		void setPosition(vec2 position) { this->position = position; }
+		void setDirection(vec2 direction) { this->direction = direction; }
+		void evaporate(float evaporationRate) { water *= (1 - evaporationRate); }
+		void adjustDirection(vec2 gradient, float inertia);
+		void adjustPosition();
+		bool isOnMap(int width, int height);
+		bool gatherSediment();
+
+	private:
+		vec2 position;
+		vec2 direction;
+		float velocity;
+		float water;
+		float sediment;
+		float capacity;
 	};
 }
